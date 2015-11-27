1b17ba790e62413a207bdd1113bc93d0c8c170c1
Olly.W <yonaichin@gmail.com>
2015-11-27 17:43:18 +0800

    add Todo Component

--- js/app.js
+++ js/app.js
@@ -1,12 +1,66 @@
-console.log('app.js loaded');
-console.log('React',React);
 var Todo = React.createClass({
   displayName:'TODO',
-  render:function(){
-    console.log('Todo render');
+  getInitialState:function(){
     return(
-      <div>Todo</div>
+      {
+        todos:[]
+      }
     );
+  },
+  addTodoHandler:function(todo){
+    var tmpTodos = this.state.todos
+    tmpTodos.push(todo);
+    this.setState({'todos':tmpTodos});
+  },
+  render:function(){
+    return(
+      <div>
+        <h1>Todo</h1>
+        <TodoInputField addTodoHandler={this.addTodoHandler}/>
+        <TodoContent todos={this.state.todos} />
+      </div>
+    );
+  }
+});
+var TodoContent = React.createClass({
+  displayName:'TODO_CONTENT',
+  getInitialState:function(){
+    return(
+      {
+        'todos':this.props.todos
+      }
+    );
+  },
+  componentWillReceiveProps:function(nextProps){
+    this.setState({'todos':nextProps.todos});
+  },
+  render:function(){
+    var todos = _.map(this.state.todos,function(todo,idx){
+      return(
+        <li key = {idx}>{todo}</li>
+      );
+    });
+    return(
+      <ul>
+        {todos}
+      </ul>
+    );
+  }
+});
+var TodoInputField = React.createClass({
+  displayName:'TODO_INPUT_FILED',
+  addTodo:function(e){
+    if(e.keyCode == 13){
+      var inputValue = ReactDOM.findDOMNode(this).value;
+      var timestamp = ' @ ' + (new Date()).toLocaleString()
+      this.props.addTodoHandler(inputValue + timestamp);
+    }
+  },
+  render:function(){
+    return(
+      <input placeholder='Type Something' type="text" onKeyDown={this.addTodo}/>
+    );
+
   }
 });
 
